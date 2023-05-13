36540c693b23db2dd1971208777ff65aaf3f8c37
Joe Chavez <joe@izen.me>
2023-05-13 13:45:00 -0700

    Initial Commit

---
+++ U2 The Fly Screen Saver/U2_The_Fly_Screen_SaverView.m
@@ -0,0 +1,51 @@
+//
+//  U2_The_Fly_Screen_SaverView.m
+//  U2 The Fly Screen Saver
+//
+//  Created by Joe Chavez on 5/13/23.
+//
+
+#import "U2_The_Fly_Screen_SaverView.h"
+
+@implementation U2_The_Fly_Screen_SaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
